6b603da409395a104caf37c87f412522b03cb999
cherryfox <hey@imjam.es>
2025-03-23 17:22:39 +0000

    Changes to neovim indentation

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -14,9 +14,10 @@ vim.opt.rtp:prepend(lazypath)
 
 -- General settings configuration
 
-vim.opt.number = true
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.number = true
 vim.opt.smartindent = true
 vim.opt.ignorecase = true
 
